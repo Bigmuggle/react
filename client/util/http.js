511103e8a2c4e6dae6fd2d7b1526cfa18af1b6e8
zhaosan <897306136@qq.com>
2018-11-21 17:53:42 +0800

    封装http.js

---
+++ client/util/http.js
@@ -0,0 +1,37 @@
+/* eslint-disable prefer-destructuring */
+import axios from 'axios'
+
+const baseUrl = (url, params) => {
+  const str = Object.keys(params).reduce((result, key) => {
+    // eslint-disable-next-line no-param-reassign
+    result +=`${key}=${params[key]}&`
+    return result
+  }, '')
+  return `${url}${str.substr(0, str.length-1)}`
+}
+
+export const get = (url, params) => new Promise((resolve, reject) => {
+  axios.get(baseUrl(url, params))
+    .then((resp) => {
+      // eslint-disable-next-line prefer-destructuring
+      const data = resp.data
+      if (data && data.success ===true) {
+        resolve(data)
+      } else {
+        reject(data)
+      }
+    }).catch(reject)
+})
+export const post = (url, params, data) => new Promise((resolve, reject) => {
+  axios.post(baseUrl(url, params), data)
+    .then((resp) => {
+      // eslint-disable-next-line prefer-destructuring
+      // eslint-disable-next-line no-shadow
+      const data = resp.data
+      if (data && data.success ===true) {
+        resolve(data)
+      } else {
+        reject(data)
+      }
+    }).catch(reject)
+})
